949ec5262521fb72fa5ecf3949da9f0e361459e8
Netherman <1159794189@qq.com>
2021-03-25 23:28:39 +0800

    initial commit

--- codes/App_frontEnd/src/pages/demopage/index.js
+++ codes/App_frontEnd/src/pages/demopage/index.js
@@ -7,13 +7,24 @@ import {
   useClearByFocusCell,
 } from 'react-native-confirmation-code-field';
 import MusicPlayer from '../../utils/MusicPlayer';
+import { LogLevel, RNFFmpeg, RNFFprobe } from 'react-native-ffmpeg';
+import RNFS from 'react-native-fs';
 
 
 
 
-class App extends Component {
 
+
+class App extends Component {  
+  
     render() {
+      const ffmpegWatermarkCommand = '-i ' + RNFS.ExternalStorageDirectoryPath+ '/test/wx_camera_1582897957311.mp4 ';
+
+
+        RNFFmpeg.execute(ffmpegWatermarkCommand).then(result => console.log(`FFmpeg process exited with rc=${result}.`));
+        //RNFFprobe.getMediaInformation(movie).then(information => {
+          //console.log('Result: ' + JSON.stringify(information));
+      //});
         return (
             <View>
                 {/*<Text>fuck my ass</Text>*/}
