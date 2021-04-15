2d601f7860e8f53c5c3ae3b44415f134b49a0039
nethermanpro <75082385+nethermanpro@users.noreply.github.com>
2021-04-15 15:14:29 +0800

    Delete codes/App_frontEnd directory

--- codes/App_frontEnd/src/utils/stylesKits.js
+++
@@ -1,9 +0,0 @@
-import {Dimensions} from "react-native";
-
-// 用来处理px到dp的单位转换问题
-//屏幕的宽与高
-export const screenWidth = Dimensions.get("window").width;
-export const screenHeight = Dimensions.get("window").height;
-//单位转换
-export const pxToDp =(elePx)=>screenWidth * elePx / 375;
-// 除以设计稿的宽度
