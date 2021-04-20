2fb61f3edcd673aaf31187263040ba6b26b98d4f
CLiang-git <liang-chen@sjtu.edu.cn>
2021-04-20 16:17:15 +0800

    addfile

---
+++ codes/BackEnd/main/java/com/example/audio/login/login.java
@@ -0,0 +1,24 @@
+package com.example.audio.login;
+
+import com.example.audio.greeting.Greeting;
+import com.example.audio.jsonReceive.jsonController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class login {
+    private static Logger login = LoggerFactory.getLogger(jsonController.class);
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public String greeting(@RequestParam(value = "name", defaultValue = "defaultname") String name, @RequestParam(value = "id", defaultValue = "defaultid") String id) {
+
+        login.info("*** login "+"name:"+name+" id:"+id+"\n");
+
+        return name+id;
+    }
+}
