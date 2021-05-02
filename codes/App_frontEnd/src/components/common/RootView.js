bba3706b8d20668c17351fe01fb64c0c09194886
Netherman <1159794189@qq.com>
2021-05-02 20:16:51 +0800

    loading

---
+++ codes/App_frontEnd/src/components/common/RootView.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { StyleSheet, AppRegistry, View, Text } from 'react-native';
+import Loading from './Loading';
+const originRegister = AppRegistry.registerComponent;
+AppRegistry.registerComponent = (appKey, component) => {
+    return originRegister(appKey, function () {
+        const OriginAppComponent = component();
+        return class extends Component {
+            render() {
+                return (
+                    <View style={styles.container}>
+                        <OriginAppComponent />
+
+                        {/* //加载动画 */}
+                        <Loading></Loading>
+                    </View>
+                );
+            };
+        };
+    });
+};
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        position: 'relative',
+    },
+});
